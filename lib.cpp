8e98196ff8553632928171f4063de3135686e91e
Andres-Vasquezunige <116440246+Andres-Vasquezunige@users.noreply.github.com>
2022-12-12 20:30:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,9 +1,9 @@
 #include "lib.h"
 float massimo(float a[], int h) {
     float d=1;
-    for (int i=0; i<h; i++){
-        if(a[i]>d){
-            d=a[i];
+    for (int j=0; j<h; j++){
+        if(a[j]>d){
+            d=a[j];
         }
     }
     return d;
