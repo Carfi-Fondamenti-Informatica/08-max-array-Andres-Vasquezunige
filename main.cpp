5753f80a8f4adfb7fcc4641d6cdb1dec66020dea
Andres-Vasquezunige <116440246+Andres-Vasquezunige@users.noreply.github.com>
2022-12-12 20:22:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    int h;
+    cin>>h;
+    float a[h];
+    float x;
+    for(int i=0; i<h; i++){
+        cin>>x;
+        a[i]=x;
+    }
+    cout<<massimo(a,h);
+
+    return 0;
 }
